516f4d6d1fcd3a23f931722da6f4dd01cd314ba9
dhanunjaya <dhanunjay.suppa@darwinbox.io>
2025-02-20 16:39:40 +0530

    removed mongo url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 // Replace this with your MongoDB Atlas connection string
-const uri = "mongodb+srv://dhanunjaysuppa:12345@cluster0.t97pt5y.mongodb.net/navispace_prac?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = process.env.MONGODB_URI;
 // Connect to MongoDB Atlas
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
